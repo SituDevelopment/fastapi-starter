d94f553ca8b9cc10f59b167daf027ce6f7a0857d
William Sawyer <wmsawyer2609@gmail.com>
2024-01-03 17:38:56 +1000

    Ensure Admin dependency provides database session

--- src/fastapi_starter/dependencies/admin.py
+++ src/fastapi_starter/dependencies/admin.py
@@ -1,11 +1,13 @@
 """Grants administrators permissions to resources."""
 
 
-from fastapi import HTTPException, status
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
 
 from ..controllers.organisations import get_organisation
 from ..schemas.organisations import PermissionType
 from ..schemas.users import User
+from .database import database
 
 
 class Admin:
@@ -13,8 +15,8 @@ class Admin:
     Requires the user to be a member of an organisation with administrative permissions.
     """
 
-    def __call__(self, user: User) -> User:
-        organisation = get_organisation(user.organisation)
+    def __call__(self, user: User, session: Session = Depends(database)) -> User:
+        organisation = get_organisation(session, user.organisation)
 
         if organisation.permission == PermissionType.ADMIN:
             return user
